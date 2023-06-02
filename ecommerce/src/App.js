4771322fe4ebefe17614332406030dc963d84284
Dana Sharon <whosedreamisthis@gmail.com>
2023-06-02 14:46:21 -0700

    categories

--- ecommerce/src/App.js
+++ ecommerce/src/App.js
@@ -1,25 +1,36 @@
 import logo from './logo.svg';
-import './App.css';
-
+import Category from "./components/category/category.component.jsx"
 function App() {
+  const categories = [{
+    id: 1,
+    title: "Hats"
+  },
+  {
+    id: 2,
+    title: "Jackets"
+  },
+  {
+    id: 3,
+    title: "Sneakers"
+  },
+  {
+    id: 4,
+    title: "Womens"
+  },
+  {
+    id: 5,
+    title: "Mens"
+  }
+];
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   categories.map((category)=>{
+    return <Category key={category.id } title={category.title}/>
+   })
   );
+      
+      
+  
+  
 }
 
 export default App;
---
+++ ecommerce/src/components/category/category.component.jsx
@@ -0,0 +1,13 @@
+
+
+const Category = ({title})=>{
+    return (<div className='category-container'>
+    <div className="background-img"> </div>  
+    <div className='category-body-container'>
+    <h2>{title}</h2>
+    <p>Shop now</p>
+    </div>
+    </div>)
+}
+
+export default Category;
