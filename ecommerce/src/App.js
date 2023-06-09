f4f074c9f20e986a4580e1d7c2a040932252930c
Dana Sharon <whosedreamisthis@gmail.com>
2023-06-08 19:29:37 -0700

    categories

--- ecommerce/src/App.js
+++ ecommerce/src/App.js
@@ -1,36 +1,35 @@
-import logo from './logo.svg';
-import Category from "./components/category/category.component.jsx"
+import logo from "./logo.svg";
+import "./App.css";
+import Directory from "./components/directory/directory.component";
 function App() {
-  const categories = [{
-    id: 1,
-    title: "Hats"
-  },
-  {
-    id: 2,
-    title: "Jackets"
-  },
-  {
-    id: 3,
-    title: "Sneakers"
-  },
-  {
-    id: 4,
-    title: "Womens"
-  },
-  {
-    id: 5,
-    title: "Mens"
-  }
-];
-  return (
-   categories.map((category)=>{
-    return <Category key={category.id } title={category.title}/>
-   })
-  );
-      
-      
-  
-  
+  const categories = [
+    {
+      id: 1,
+      title: "hats",
+      imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
+    },
+    {
+      id: 2,
+      title: "jackets",
+      imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
+    },
+    {
+      id: 3,
+      title: "sneakers",
+      imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
+    },
+    {
+      id: 4,
+      title: "womens",
+      imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
+    },
+    {
+      id: 5,
+      title: "mens",
+      imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
+    },
+  ];
+  return <Directory categories={categories} />;
 }
 
 export default App;
